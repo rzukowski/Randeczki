79a24f3b389f7ffd218fca57c739a87adb030c15
Rafał Żukowski <rafal.zukowski@op.pl>
2013-03-15 21:37:25 +0100

    Jeszcze jeden commit

--- App_Code/Usr.cs
+++ App_Code/Usr.cs
@@ -12,8 +12,11 @@ using System.Data.SqlClient;
 /// </summary>
 public class Usr
 {
+ //connection string pobrany z web.config
     public static string ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["FriendsConnectionString"].ConnectionString;// @"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\Raf\Documents\Visual Studio 2012\WebSites\WebSite5\App_Data\ASPNETDB.MDF;Integrated Security=True;User Instance=True";
-		 public static bool AddFriend(String userid, String friendid)
+		 
+    //dodanie do ulubionych - dodanie do tabeli friends
+    public static bool AddFriend(String userid, String friendid)
     {
         SqlConnection con = new SqlConnection(ConnectionString);
         try
@@ -37,7 +40,7 @@ public class Usr
     }
 
 
-
+    //wysłanie wiadomości - zapis do tabeli message
 
     public static bool SendMssg(String fromuserid, String touserid ,  String message)
     {
@@ -63,6 +66,7 @@ public class Usr
         }
     }
 
+    //usunięcie z listy ulubionych
     public static bool DeleteFriend(String userid, String friendid)
     {
         SqlConnection con = new SqlConnection(ConnectionString);
@@ -85,7 +89,7 @@ public class Usr
             con.Close();
         }
     }
-
+    //usuwanie wiadomości
     public static bool DeleteMssg(String msgid)
     {
         SqlConnection con = new SqlConnection(ConnectionString);
@@ -106,6 +110,83 @@ public class Usr
         {
             con.Close();
         }
+    }
+
+
+    public static bool IncrementNotification(String userid)
+    {
+        SqlConnection con = new SqlConnection(ConnectionString);
+        try
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("UPDATE notifications SET numberOfNotifications=(numberOfNotifications + 1) WHERE userid=@userid",con);
+            cmd.Parameters.AddWithValue("@userid", userid);
+            cmd.ExecuteNonQuery();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            HttpContext.Current.Trace.Write(ex.Message);
+            return false;
+
+        }
+        finally
+        {
+            con.Close();
+        }
+
+    }
+
+    public static bool DecrementNotification(String userid)
+    {
+        SqlConnection con = new SqlConnection(ConnectionString);
+        try
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("update notifications set numberOfNotifications=0 where userid=@userid",con);
+            cmd.Parameters.AddWithValue("@userid", userid);
+            cmd.ExecuteNonQuery();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            HttpContext.Current.Trace.Write(ex.Message);
+            return false;
+
+        }
+        finally
+        {
+            con.Close();
+        }
+
+    }
+
+    public static int NumberOfNotifications(String userid)
+    {
+        SqlConnection con = new SqlConnection(ConnectionString);
+        try
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select numberOfNotifications from notifications where userid=@userid",con);
+            cmd.Parameters.AddWithValue("@userid", userid);
+            SqlDataReader myReader = cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+            myReader.Read();
+            int number = myReader.GetInt32(0);
+            if (number > 0) return number;
+            else return 0;
+        }
+        catch (Exception ex)
+        {
+            HttpContext.Current.Trace.Write(ex.Message);
+            return 0;
+
+        }
+        finally
+        {
+            con.Close();
+        }
+
+
     }
 
 	}
